317732f650e6a05774f89ead3e70b73d90812214
Masha <masha@MacBook-Pro-Masha.local>
2022-05-31 17:14:06 +0300

    another fix

--- src/mylib/main.py
+++ src/mylib/main.py
@@ -25,11 +25,12 @@ def main():
     csv_path=os.path.normpath(args.csv_path)
     x_coord, y_coord, z_coord, columns, result = detecter(video_path)
     #calculate similarity for every point
-    df_1=pd.read_csv(args.keypoints_1)
-    df_2=pd.read_csv(args.keypoints_2)
-    all_dist=[]
-    all_dist.append(calculate_score(df_1, df_2, col_n))
-    print(all_dist)
+    if args.keypoints_1 and args.keypoints_2:
+        df_1=pd.read_csv(args.keypoints_1)
+        df_2=pd.read_csv(args.keypoints_2)
+        all_dist=[]
+        all_dist.append(calculate_score(df_1, df_2, col_n))
+        print(all_dist)
 
 
 if __name__ == '__main__':
